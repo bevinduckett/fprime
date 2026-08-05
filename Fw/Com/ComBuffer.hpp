b919a496760321be6b41223bd4092a9248ab2eea
Bevin K Duckett <username@://github.com>
2026-08-05 14:35:31 -0700

    Created ComBuffer class to resolve copilot finding from PR 5582

--- Fw/Com/ComBuffer.hpp
+++ Fw/Com/ComBuffer.hpp
@@ -7,11 +7,36 @@
 #ifndef FW_COM_BUFFER_HPP
 #define FW_COM_BUFFER_HPP
 
+#include <cstring>
 #include "Fw/Types/LinearBufferTemplate.hpp"
 
 namespace Fw {
 
-using ComBuffer = LinearBufferTemplate<FW_COM_BUFFER_MAX_SIZE>;
+class ComBuffer final : public LinearBufferTemplate<FW_COM_BUFFER_MAX_SIZE> {
+  public:
+    ComBuffer() { std::memset(this->getBuffAddr(), 0, this->getCapacity()); }
+
+    ComBuffer(const U8* args, FwSizeType size) : LinearBufferTemplate<FW_COM_BUFFER_MAX_SIZE>(args, size) {
+        const FwSizeType dataSize = this->getSize();
+        std::memset(this->getBuffAddr() + dataSize, 0, this->getCapacity() - dataSize);
+    }
+
+    ComBuffer(const ComBuffer& other) : LinearBufferTemplate<FW_COM_BUFFER_MAX_SIZE>(other) {
+        const FwSizeType dataSize = this->getSize();
+        std::memset(this->getBuffAddr() + dataSize, 0, this->getCapacity() - dataSize);
+    }
+
+    ComBuffer& operator=(const ComBuffer& other) {
+        if (this != &other) {
+            LinearBufferTemplate<FW_COM_BUFFER_MAX_SIZE>::operator=(other);
+            const FwSizeType dataSize = this->getSize();
+            std::memset(this->getBuffAddr() + dataSize, 0, this->getCapacity() - dataSize);
+        }
+        return *this;
+    }
+
+    ~ComBuffer() override = default;
+};
 
 }  // namespace Fw
 
